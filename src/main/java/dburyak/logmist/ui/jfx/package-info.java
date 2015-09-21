430e03cafe073e66c8c7121dd195c40b33928c47
Dmytro Buryak <dmytro.buryak@gmail.com>
2015-09-21 10:35:41 +0300

    Fix statusBar layout

--- src/main/java/dburyak/logmist/ui/jfx/package-info.java
+++ src/main/java/dburyak/logmist/ui/jfx/package-info.java
@@ -1,5 +1,9 @@
 /**
- * @author Андрей
- *
+ * Project : logmist.<br/>
+ * JFX GUI implementaion.
+ * <br/><b>Created on:</b> <i>3:48:46 AM Sep 20, 2015</i>
+ * 
+ * @author <i>Dmytro Buryak &ltdmytro.buryak@gmail.com&gt</i>
+ * @version 0.1
  */
 package dburyak.logmist.ui.jfx;
